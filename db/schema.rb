fc5b53173037538eadc5da5b23cb4426bc00ec3d
Xien Thomas <xien.thomas@tamu.edu>
2018-03-27 13:39:41 +0000

    deploy to heroku| tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312212252) do
+ActiveRecord::Schema.define(version: 20180324082735) do
 
   create_table "questions", force: :cascade do |t|
     t.string "content"
